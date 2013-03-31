3bd8e4351192d83c816985017325e79dab76bff2
Jori Lallo <jtlallo@gmail.com>
2013-03-31 15:58:53 -0700

    Comments

--- example/example/api/urls.py
+++ example/example/api/urls.py
@@ -3,9 +3,13 @@ from django.conf.urls import patterns, url
 from example.api.views import *
 
 urlpatterns = patterns('api.views',
+    # Simple helper function to echo data back
     url(r'^echo/?$', EchoView.as_view(), name='api_echo'),
+    
+    # User account - Returns API key
     url(r'^account/?$', AccountView.as_view(), name='api_account'),
     
+    # Sample API
     url(r'^users/?$', UsersView.as_view(), name='api_users'),
     url(r'^users/(?P<user_id>[A-Za-z0-9_]+)/?$', UserView.as_view(), name='api_user'),
 )
